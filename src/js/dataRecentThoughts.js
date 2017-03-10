27da124da8cc845fc7c938f4eada07d4e71fc2e7
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-03-10 16:52:36 -0500

    Recent Thoughts is colelcting and outputting data to the page

--- src/js/dataRecentThoughts.js
+++ src/js/dataRecentThoughts.js
@@ -2,19 +2,16 @@
     'use strict';
 
     window.thoughter = window.thoughter || {};
-
     console.log("hello from recenthoughts!!!");
 
     fetch('https://thoughter.herokuapp.com/api/thoughts?filter={"order":"createTime DESC", "limit": 20}',
 
-        {
+          {
             method: 'GET',
             headers: {
                 Authorization: 'IuiL0gsrlxnlPIZQDp6gAZZVlmNlfrTpMgmVIRJIq08DPFxyq5FRrCBh4k55LOC6',
                 'Content-Type': 'application/json'
-
-
-            }
+              }
         }
 
     ).then(function handleResponse(response) {
@@ -22,54 +19,32 @@
             console.log("SUCCESS " + response.status);
 
             response.json().then(function printData(thoughts) {
-                // console.log(thoughts[0].content);
                 // console.log(thoughts); //this returns the thought data object
-                //next step will be to retrurn each object inside the object array from
-                // a for each loop, populating the panels inside li's within the html
-                // using jquery
-
-
                 thoughts.forEach(function getThoughtAndDate(data) {
                     console.log(data);
 
-
+                      let newDate = Date(data.createTime);//turn this into a date object so we manipulate it
+                      console.log(newDate);
                       let newArticle = $('<article></article>');
                         newArticle.addClass('pabel-body').text(data.content);
 
                       let newAside = $('<aside></aside>');
-                      newAside.addClass('panel-heading').text(data.createTime);
+                      newAside.addClass('panel-heading').text("Posted at " + newDate);
 
                       let newLi = $('<li></li');
                       newLi.addClass('panel panel-default').append(newAside, newArticle);
 
-
                       $('ul').append(newLi);
 
+                    });
 
-                });
+                  });
 
+                } else {
+                  console.log('ERROR- ' + response.status);
+
+                }
 
               });
 
-
-        } else {
-
-            console.log('ERROR- ' + response.status);
-
-
-
-        }
-
-
-
-
-    });
-
-
-
-
-
-
-
-
-}());
+            }());
