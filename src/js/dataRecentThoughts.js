0441ea9dba90de8c26d39b9bc31b42f99715d0f8
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-03-10 11:40:31 -0500

    dataRecentThoughts is fetching data from the server and returning objectData

--- src/js/dataRecentThoughts.js
+++ src/js/dataRecentThoughts.js
@@ -3,22 +3,37 @@
 
     window.thoughter = window.thoughter || {};
 
-    // console.log ("hello from recenthoughts!!!");
-    //
+    console.log("hello from recenthoughts!!!");
 
-    // fetch( 'https://thoughter.herokuapp.com/api/thoughts',
 
-    //     {
-    //         method: 'GET',
-    //         headers:{
-    //            Authorization:  /*"authorization id key??/"*/
-    //
-    //
-    //        }
-    //      }
-    //
-    // );
+    fetch('https://thoughter.herokuapp.com/api/thoughts',
 
+        {
+            method: 'GET',
+            headers: {
+                Authorization: 'IuiL0gsrlxnlPIZQDp6gAZZVlmNlfrTpMgmVIRJIq08DPFxyq5FRrCBh4k55LOC6',
+                'Content-Type': 'application/json'
+
+
+            }
+        }
+
+    ).then(function handleResponse(response){
+      //check response code
+
+
+
+        response.json().then(function printData(thoughts){
+              console.log(thoughts[0].content);//this returns the thought data object
+              //next step will be to retrurn each object inside the object array from
+              // a for each loop, populating the panels inside li's within the html
+              // using jquery
+              //
+
+        });
+
+
+    });
 
 
 
