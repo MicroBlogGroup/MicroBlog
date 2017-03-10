f42a6dbc7b64497721e09fc1e499931b142db1c1
elizabeth boles <elizabethboles@elizabeths-MacBook-Air.local>
2017-03-09 22:32:41 -0500

    success on login post using ajax and name space

--- src/js/data-login.js
+++ src/js/data-login.js
@@ -1,18 +1,22 @@
 (function() {
   'use strict';
   window.thoughter = window.thoughter || {};
+  //in our name space variable the property login = the function log in on this file
+  window.thoughter.login = login;
 console.log('hi');
  function login(username, password){
    //creating ajax for log in ajax passes data through objects
   $.ajax({
-    url: 'https://thoughter.herokuapp.com/api',
+    url: 'https://thoughter.herokuapp.com/api/Authors/login',
     method: 'POST',
     dataType: 'json',
     //creating data block/body which are always objects
-    data: {
-      'email': username,
+    //server doesnt take any data type other then json so we are making this data
+    //block into json
+    data: JSON.stringify({
+      'username': username,
       'password': password,
-    },
+    }),
     headers: {
        'Content-Type': 'application/json'
     }
@@ -25,6 +29,5 @@ console.log('hi');
     .fail(function error(err){
       console.log(err);
     });
-
 }
 }());
--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,22 @@
 (function() {
   'use strict';
   window.thoughter = window.thoughter || {};
-  console.log('hello world');
-  console.log($('h1'));
+  // console.log('hello world');
+  // console.log($('h1'));
+  //$ means im using jquery
+  //setting up event handler for login submit
+  $('.login').on('submit', function login(e){
+    //the events default is to reset the form we dont want it to reset because
+    //we still have stuff we want to do with the values
+    e.preventDefault();
+    //once this is submited do some stuff - collect the values from the inputs
+    let username = $('#userName')[0].value;
+    console.log(username);
+    let password = $('#exampleInputPassword1')[0].value;
+    console.log(password);
+    //now im going to the other js and passing the other file the log in
+    //now calling the function
+    window.thoughter.login(username, password);
+  });
 
 }());
